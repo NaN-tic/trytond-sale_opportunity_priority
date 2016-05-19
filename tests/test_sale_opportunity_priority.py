2d8093913bce826b4a93a9e7070495391f20d1de
Raimon Esteve <raimon@nan-tic.com>
2016-05-19 10:04:21 +0200

    Rename Test Class name according module name

--- tests/test_sale_opportunity_priority.py
+++ tests/test_sale_opportunity_priority.py
@@ -5,12 +5,13 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(ModuleTestCase):
-    'Test module'
+class SaleOpportunityPriorityTestCase(ModuleTestCase):
+    'Test Sale Oopportunity Priority module'
     module = 'sale_opportunity_priority'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        SaleOpportunityPriorityTestCase))
     return suite
