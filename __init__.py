4fdfa5acd6bac5a92f86c49a3be8084e6a6d7ae7
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-05 00:56:32 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .opportunity import *
+from . import opportunity
 
 def register():
     Pool.register(
-        Priority,
-        Opportunity,
+        opportunity.Priority,
+        opportunity.Opportunity,
         module='sale_opportunity_priority', type_='model')
